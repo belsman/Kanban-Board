accc9d38ccbe8b7d90507fad1230556f3763b20a
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-26 14:45:28 +0100

    Redesign the models

--- board/models.py
+++ board/models.py
@@ -1,34 +1,25 @@
 from django.db import models
 
 
-class Project(models.Model):
-    """Scrum-style project management."""
+class Board(models.Model):
+    """Board for managing task"""
 
     name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank='', default='')
 
     def __str__(self):
         return self.name
 
 
-class Sprint(models.Model):
-    """Development iteration period."""
-
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank='', default='')
-    end = models.DateField(unique=True)
-    project = models.ForeignKey(Project, models.CASCADE)
-
-    def __str__(self):
-      return self.name
-
-
-class State(models.Model):
+class List(models.Model):
     """A current state of the task. (To-Do, In-Prgress, Done) etc.""" 
 
     name = models.CharField(max_length=100)
     order = models.SmallIntegerField(default=0)
-    sprint = models.ForeignKey(Sprint, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
@@ -36,16 +27,13 @@ class Task(models.Model):
 
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, default='')
-    # When a task does not belong to belong to a sprint and task, it is a project backlog!
-    sprint = models.ForeignKey(Sprint, blank=True, null=True, on_delete=models.CASCADE)
-    state = models.ForeignKey(State, blank=True, null=True, on_delete=models.CASCADE)
-    #
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    list = models.ForeignKey(List, blank=True, null=True, on_delete=models.CASCADE)
     is_backlog = models.BooleanField()
     order = models.SmallIntegerField(default=0)
     started = models.DateTimeField(blank=True, null=True)
     completed = models.DateTimeField(blank=True, null=True)
-    completion_hours = models.PositiveIntegerField()
+    points = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
