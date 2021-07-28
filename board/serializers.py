c33a23d09c7e482739192a22272805b220afa7c6
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-28 20:59:14 +0100

    Add serializers for Tasks and List models

--- board/serializers.py
+++ board/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from board.models import Board
+from board.models import Board, List, Task
 
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
-        fields = ['name', 'description']
+        fields = ['id', 'name', 'description']
+
+
+class ListSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = List
+        fields = ['id', 'name', 'board']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Task
+        fields = ['id', 'title', 'description', 'board', 'list', 'order', 'started', 'completed']
