31506d065f084e8ed408221a52b1a88e207dcebd
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-28 23:35:54 +0100

    Add nested model serialization to list and board

--- board/serializers.py
+++ board/serializers.py
@@ -2,21 +2,21 @@ from rest_framework import serializers
 from board.models import Board, List, Task
 
 
-class BoardSerializer(serializers.ModelSerializer):
-    # return related list
-    class Meta:
-        model = Board
-        fields = ['id', 'name', 'description']
-
-
-class ListSerializer(serializers.ModelSerializer):
-    # return related tasks
-    class Meta:
-        model = List
-        fields = ['id', 'name', 'order', 'board']
-
-
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'board', 'list', 'order', 'started', 'completed']
+
+
+class ListSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+    class Meta:
+        model = List
+        fields = ['id', 'name', 'order', 'board', 'tasks']
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    lists = ListSerializer(many=True, read_only=True)
+    class Meta:
+        model = Board
+        fields = ['id', 'name', 'description', 'lists']
