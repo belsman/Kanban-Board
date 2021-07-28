376c2eb29561cb7f64905990c6df29d087b1b7e3
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-28 20:39:13 +0100

    Add board serializer

---
+++ board/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from board.models import Board
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['name', 'description']
