403c48fc6994f654bcc979018f4437b9e098d21b
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-30 00:11:55 +0100

    Add CRUD operations for List

--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,9 @@ from board import views
 urlpatterns = [
     path('boards/', views.BoardList.as_view()),
     path('boards/<int:pk>/', views.BoardDetail.as_view()),
+
+    path('lists/', views.ListCreation.as_view()),
+    path('lists/<int:pk>/', views.ListDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- board/views.py
+++ board/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from board.models import Board
-from board.serializers import BoardSerializer
+from rest_framework import mixins
+from board.models import Board, List
+from board.serializers import BoardSerializer, ListSerializer
 
 
 class BoardList(generics.ListCreateAPIView):
@@ -12,3 +13,13 @@ class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+
+
+class ListCreation(generics.CreateAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+
+class ListDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
