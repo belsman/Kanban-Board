55331a6c90f96cd9d8d7612ca5a45be07ca0a0ab
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-31 11:53:35 +0100

    Populate the creator fields with the current user

--- board/views.py
+++ board/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework import mixins
 from board.models import Board, List, Task
 from board.serializers import BoardSerializer, ListSerializer, TaskSerializer
 
@@ -7,6 +6,9 @@ from board.serializers import BoardSerializer, ListSerializer, TaskSerializer
 class BoardList(generics.ListCreateAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
 
 class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -18,6 +20,9 @@ class ListCreation(generics.CreateAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
 
 class ListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = List.objects.all()
@@ -27,6 +32,9 @@ class ListDetail(generics.RetrieveUpdateDestroyAPIView):
 class TaskCreation(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
