61c58092fc71b2e668d8dffdc6177fd8253c7aff
Bello Babakolo <fleetbeekay@gmail.com>
2021-07-31 11:48:46 +0100

    Add creators to the serializers for Task, List, Model

--- board/serializers.py
+++ board/serializers.py
@@ -3,23 +3,29 @@ from board.models import Board, List, Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    creator = serializers.ReadOnlyField(source='creator.username')
+    
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'board', 'list', 'order', 'started', 'completed']
+        fields = ['id', 'title', 'description', 'board', 'list', 'order', 'started', 'completed', 'creator']
 
 
 class ListSerializer(serializers.ModelSerializer):
+    creator = serializers.ReadOnlyField(source='creator.username')
     tasks = TaskSerializer(many=True, read_only=True)
+
     class Meta:
         model = List
-        fields = ['id', 'name', 'order', 'board', 'tasks']
+        fields = ['id', 'name', 'order', 'creator', 'board',  'tasks']
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    creator = serializers.ReadOnlyField(source='creator.username')
     lists = ListSerializer(many=True, read_only=True)
+
     class Meta:
         model = Board
-        fields = ['id', 'name', 'description', 'lists']
+        fields = ['id', 'name', 'description', 'creator', 'lists']
 
     def create(self, validated_data):
         board = Board.objects.create(**validated_data)
