3f6c00ca3de015ee577cfc1c527ed840c9c96e40
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-02 20:08:25 +0100

    Configure token authentication for board

--- kanban/urls.py
+++ kanban/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('', include('board.urls')),
     path('api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns += [
+    path('api-token-auth/', views.obtain_auth_token)
 ]
