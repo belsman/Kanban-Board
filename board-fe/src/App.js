2853b115baa272717358252d6786f11b98c5c42c
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-07 13:25:50 +0100

    Add basic board feature

--- board-fe/src/App.js
+++ board-fe/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './features/authentication/Login';
 import Register from './features/authentication/Register';
+import Board from './features/board/Board';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={Board} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ board-fe/src/features/board/Board.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Board() {
+  
+  return (
+    <h1>Current board!: Welcome to your board!</h1>
+  );
+}
+
+export default Board;
