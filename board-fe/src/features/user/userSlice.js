63c3bc4520e3f61ba63dcf95e993205773cbd0ba
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-13 15:06:49 +0100

    create thunks for fetching users

--- board-fe/src/features/user/userSlice.js
+++ board-fe/src/features/user/userSlice.js
@@ -1,11 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {};
+const initialState = {
+  status: 'idle',
+  error: null,
+  user: null,
+};
+
+export const fetchUser = createAsyncThunk('user/fetchUser', async () => {
+  const { data } = await axios.get("/auth/user");
+  return data;
+});
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: {
+    [fetchUser.pending]: (state, action) => {
+      state.status = 'loading'
+    },
+    [fetchUser.fulfilled]: (state, action) => {
+      state.status = 'completed';
+      state.user = action.payload;
+    },
+    [fetchUser.rejected]: (state, action) => {
+      action.status = 'failure';
+      state.error = action.error.message;
+    }
+  }
 });
 
 export default userSlice.reducer;
