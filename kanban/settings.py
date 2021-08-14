a41e892b3d3994f7ca99c9845c516778f8015ac8
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-14 14:16:31 +0100

    Use default session/token authentication

--- kanban/settings.py
+++ kanban/settings.py
@@ -134,9 +134,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 CORS_ORIGIN_WHITELIST = [
