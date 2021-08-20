ee22f72ddfc15af4bf6fee6eee340ca0af275ace
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-20 22:10:55 +0100

    Handle the loading of fetchBoards

--- board-fe/src/features/board/BoardList.js
+++ board-fe/src/features/board/BoardList.js
@@ -4,15 +4,26 @@ import { useDispatch, useSelector } from 'react-redux';
 function BoardList() {
   const dispatch = useDispatch();
 
+  const boards = useSelector(state => state.boards.data);
+  const boardStatus = useSelector(state => state.boards.status);
+  const error = useSelector(state => state.boards.error);
+
   useEffect(() => {
     dispatch(fetchBoards())
-  }, [])
+  }, [fetchBoards, dispatch]);
 
-  const boards = useSelector(state => state.boards.data);
-  const renderedBoards = boards
-          .map(board => <li key={board.id} className="board">{board.name}</li>);
+  let content;
 
-  return <ul>{renderedBoards}</ul>
+  if (boardStatus === 'loading') {
+    content = <div className="loader">Loading....</div>;
+  } else if (boardStatus === 'succeeded') {
+    content = boards
+      .map(board => <li key={board.id} className="board">{board.name}</li>);
+  } else if (boardStatus === 'failure') {
+    content = <div>{error}</div>
+  }
+
+  return <ul>{content}</ul>
 }
 
 export default BoardList;
