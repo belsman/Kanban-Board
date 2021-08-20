080cecfa060807b0d2e2f399e09fe3c093fb4bd8
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-20 22:13:00 +0100

    Import the boardsSlice

--- board-fe/src/features/board/BoardList.js
+++ board-fe/src/features/board/BoardList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { fetchBoards } from './boardsSlice';
 
 function BoardList() {
   const dispatch = useDispatch();
