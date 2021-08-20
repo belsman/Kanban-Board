feaf1835bea01324edd7994055a9f930629f3470
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-20 22:34:56 +0100

    Connect the boardsSlice to redux-store

--- board-fe/src/app/store.js
+++ board-fe/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from '../features/authentication/AuthenticationSlice';
+import boardsSlice from '../features/board/boardsSlice';
 
 export const store = configureStore({
   reducer: {
     authUser: authenticationReducer,
+    boards: boardsSlice,
   },
 });
