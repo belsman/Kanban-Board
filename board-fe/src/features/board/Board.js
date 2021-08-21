12761d5f013b52f5ab8d338aa4d844d5a38830d0
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-21 11:02:14 +0100

    Get the board instance in the < Board />

--- board-fe/src/features/board/Board.js
+++ board-fe/src/features/board/Board.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Board({ match }) {
   const { boardId } = match.params;
 
+  const board = useSelector(state => state.boards.data
+                .find(board => board.id === Number(boardId)));
+
+  if (!board) {
+    return (
+      <section>
+        <h2>Board Not Found!</h2>
+      </section>
+    );
+  }
+
   return (
-    <div>Current board! with boardId: {boardId}</div>
+    <div>Current board! ({board.name}) with boardId: {boardId}</div>
   );
 }
 
