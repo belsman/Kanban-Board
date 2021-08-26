5de8a55dffb51b8b8e924f310d12da73e79b84f0
Bello Babakolo <fleetbeekay@gmail.com>
2021-08-26 18:36:03 +0100

    Drag element task between lists in prototype

--- prototype/js/prototype.js
+++ prototype/js/prototype.js
@@ -1,21 +1,23 @@
 const tasks = document.querySelectorAll('div.task');
 const lists = document.querySelectorAll('div.list');
-console.log(lists);
+
+const taskDragged = e => {
+  const { id, textContent } = e.target;
+  e.dataTransfer.setData('taskId', id);
+};
+
+const taskDropped = e => {
+  e.preventDefault();
+  const taskId = e.dataTransfer.getData('taskId');
+  const task = document.querySelector(`div#${taskId}`);
+  e.target.appendChild(task);
+}
 
 for (let task of tasks) {
-    task.addEventListener('dragstart', (e) => console.log("Start dragging!"));
-    // task.addEventListener('dragover', (e) => console.log("Drags over!"));
-    // task.addEventListener('drop', (e) => console.log("Payload drop!"));
+  task.addEventListener('dragstart', taskDragged);
 }
 
 for (let list of lists) {
-    //task.addEventListener('dragstart', (e) => console.log("Start dragging!"));
-    list.addEventListener('dragover', (e) => {
-        e.preventDefault();
-        console.log("over A LIST!")
-    });
-    list.addEventListener('drop', (e) => {
-        e.preventDefault();
-        console.log("payload droped");
-    });
+  list.addEventListener('dragover', e => e.preventDefault());
+  list.addEventListener('drop', taskDropped);
 }
