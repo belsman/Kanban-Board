5b3620a5d74407c76b07a04059c78934ad4e79c9
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-04 00:45:54 +0100

    Add transition to the outer model component

--- board-fe/src/components/modal/modal.js
+++ board-fe/src/components/modal/modal.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 function Modal({ show, onClose, title, children }) {
-
-  if (!show) {
-    return null;
-  }
-
   return (
-    <div className="modal" onClick={onClose}>
+    <div className={`modal ${show ? 'show': ''}`} onClick={onClose}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
         <div className="modal-header">
           <h4>{title}</h4>
