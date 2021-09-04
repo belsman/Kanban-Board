6549aa5c796ecaac90611802d4f4db412e0ac445
Bello Babakolo <fleetbeekay@gmail.com>
2021-09-04 01:22:11 +0100

    Add transition Group

--- board-fe/src/components/modal/modal.js
+++ board-fe/src/components/modal/modal.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 function Modal({ show, onClose, title, children }) {
   return (
-    <div className={`modal ${show ? 'show': ''}`} onClick={onClose}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <div className="modal-header">
-          <h4>{title}</h4>
-        </div>
-        <div className="modal-body">
-          {children}
-        </div>
-        <div className="modal-footer">
-          <button onClick={onClose} className="button">close</button>
+    <CSSTransition
+      in={show}
+      unmountOnExit
+      timeout={{ enter: 0, exit: 300 }}
+    >
+      <div className={`modal ${show ? 'show': ''}`} onClick={onClose}>
+        <div className="modal-content" onClick={e => e.stopPropagation()}>
+          <div className="modal-header">
+            <h4>{title}</h4>
+          </div>
+          <div className="modal-body">
+            {children}
+          </div>
+          <div className="modal-footer">
+            <button onClick={onClose} className="button">close</button>
+          </div>
         </div>
       </div>
-    </div>
+    </CSSTransition>
   )
 }
 
