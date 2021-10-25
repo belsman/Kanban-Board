5dc76d1310f8d632e51ff8711d03d4984c38d216
Bello Babakolo <fleetbeekay@gmail.com>
2021-10-25 22:49:08 +0100

    Remove a hardcoded Django secret key

--- kanban/settings.py
+++ kanban/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e$9h86x=$$595u_w-wpqvu%h5x!6g!hs7&q-e_nvs3^va+6p=='
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
